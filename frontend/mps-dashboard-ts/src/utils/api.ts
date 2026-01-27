5e1d25faebd34a34b8a6ddc3927bcfbf65d324b5
sakanul <lukasmoreira@ufam.edu.br>
2026-01-27 09:31:12 -0400

    feat: test

--- frontend/mps-dashboard-ts/src/utils/api.ts
+++ frontend/mps-dashboard-ts/src/utils/api.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://0.0.0.0:8000/";
+const API_URL = "http://0.0.0.0:4000/";
 
 export async function fetchWithAuth(endpoint: string, options: RequestInit = {}) {
   const token = localStorage.getItem('access_token');
